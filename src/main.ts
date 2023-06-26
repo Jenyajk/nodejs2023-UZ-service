bb0c8f973b2b2fc66313c2c95994c078a8f0d765
Jenyajk <101403726+Jenyajk@users.noreply.github.com>
2023-06-27 00:06:24 +0500

    fix: little changes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT;
+  app.useGlobalPipes(
+    new ValidationPipe({ enableDebugMessages: true, whitelist: true }),
+  );
   await app.listen(port);
 }
 
